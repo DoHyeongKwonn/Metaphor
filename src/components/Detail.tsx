034ce34ff9bb439940e67e1a90879db3456131df
DESKTOP-NMUKNOE\dolcelatte <kfh1001@gmail.com>
2024-05-30 11:34:16 +0900

    리뷰 삭제 기능 추가

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Credits, fetchMovieCredits, fetchMovieDetail, fetchMovieTrailer, Movie } from "../Api/tmdbApi";
 import Review from "./Review";
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
-import { db } from "../firebase";
+import { collection, deleteDoc, doc, onSnapshot, orderBy, query } from "firebase/firestore";
+import { auth, db } from "../firebase";
 import LikeButton from "./Like";
 
 interface Video {
@@ -19,7 +19,8 @@ function Detail() {
   const [trailer, setTrailer] = useState<Video | null>(null);
   const [loading, setLoading] = useState(true);
   const [credits, setCredits] = useState<Credits | null>(null);
-  const [reviews, setReviews] = useState<{ content: string; username: string; time: string }[]>([]);
+  const [reviews, setReviews] = useState<{ content: string; username: string; time: string; id: string }[]>([]);
+  const user = auth.currentUser;
 
   useEffect(() => {
     const fetchMovie = async () => {
@@ -42,6 +43,7 @@ function Detail() {
 
     fetchMovie();
   }, [id]);
+
   useEffect(() => {
     const fetchReviews = () => {
       if (id) {
@@ -49,12 +51,13 @@ function Detail() {
         const q = query(reviewsRef, orderBy("createdAt"));
 
         const unsubscribe = onSnapshot(q, (querySnapshot) => {
-          const reviewsList: { content: string; username: string; time: string }[] = [];
+          const reviewsList: { content: string; username: string; time: string; id: string }[] = [];
           querySnapshot.forEach((doc) => {
             reviewsList.push({
               content: doc.data().content,
               username: doc.data().username,
               time: doc.data().createdAt.toDate().toLocaleString(),
+              id: doc.id,
             });
           });
           setReviews(reviewsList);
@@ -66,6 +69,15 @@ function Detail() {
 
     fetchReviews();
   }, [id]);
+
+  const handleDeleteReview = async (reviewId: string) => {
+    try {
+      await deleteDoc(doc(db, `review/${id}/reviews`, reviewId));
+      alert("댓글이 삭제되었습니다.");
+    } catch (error) {
+      console.error("Error removing document: ", error);
+    }
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -123,31 +135,33 @@ function Detail() {
             </div>
           )}
           <LikeButton id={id} />
-          {/* <div className="indicator mt-3">
-            <span className="indicator-item badge badge-secondary top-[5px] right-[3px]"></span>
-            <button className=" text-lg ">Like</button>
-          </div> */}
-          {/* {trailer && (
-              <a
-                href={`https://www.youtube.com/watch?v=${trailer.key}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="btn btn-primary"
-              >
-                Trailer
-              </a>
-            )} */}
         </div>
       </div>
       <div className="flex flex-col items-center mt-10">
         <div className="pb-4 px-4 w-2/3">
           {reviews.length > 0 ? (
-            reviews.map((review, index) => (
-              <div key={index} className=" mb-2">
+            reviews.map((review) => (
+              <div key={review.id} className=" mb-2">
                 <div className="area rounded-lg shadow-md pb-1 min-h-15">
                   <div className="px-4 pt-2">{review.content}</div>
                   <div className="px-4 text-right">
                     {review.username} - {review.time}
+                    {user !== null && user.displayName === review.username && (
+                      <button
+                        className="btn btn-circle w-[24px] h-[24px] ml-[16px]"
+                        onClick={() => handleDeleteReview(review.id)}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-3 w-3"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                      </button>
+                    )}
                   </div>
                 </div>
               </div>
